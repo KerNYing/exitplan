b5bac9a3a873f05c5041c4444e745e6ae74ec5ce
MCYJ <june1012june@gmail.com>
2025-01-19 01:59:00 +0900

    Feat: add middleware to check token and apply for routes which is needed with authorization

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const router = express.Router();
 
+// token check 필요없음
 router.post("/signupReq", authController.signUp);
 router.post("/signin", authController.signIn);
 
